82e47b5c0a874b26abbecfe070a0a403b37da016
antonyjes <72809149@continental.edu.pe>
2022-12-20 19:12:57 -0500

    List items with axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import { useEffect, useState } from 'react';
+import useAxiosFetch from './hooks/useAxiosFetch';
 import './App.css';
 import Content from './components/Content';
 import Title from './components/Title';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const {data, fetchError, isLoading} = useAxiosFetch('http://localhost:3500/items');
+
+  useEffect(() => {
+    setItems(data);
+  }, [data])
+
+  useEffect(() => {
+    const filteredResults = items.filter((item) => ((item.task).toLowerCase()).includes(search.toLowerCase()))
+    setSearchResults(filteredResults.reverse());
+  }, [items, search])
+
   return (
     <div className="App">
       <Title title="TodoList - App"/>
-      <Content />
+      <>
+        {isLoading && (
+          <div
+            className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3"
+            role="alert"
+          >
+            <p className="font-bold">Loading data ...</p>
+          </div>
+        )}
+        {fetchError && (
+          <div role="alert">
+            <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+              Error
+            </div>
+            <div className="border border-t-0 border-red-400 rounded-b bg-red-100 px-4 py-3 text-red-700">
+              <p>{fetchError}</p>
+            </div>
+          </div>
+        )}
+        {!fetchError && !isLoading && (
+          <Content
+            items={searchResults}
+          />
+        )}
+      </>
     </div>
   );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,7 @@
 import AddItem from "./AddItem";
 import Search from "./Search";
 
-const Content = () => {
+const Content = ({ items }) => {
   return (
     <div className="py-10 h-screen bg-gray-300 px-2">
       <div className="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg overflow-hidden md:max-w-lg">
@@ -9,24 +9,33 @@ const Content = () => {
           <div className="w-full p-4">
             <AddItem />
             <Search />
-            <ul>
-              <li className="flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded transition">
-                <div className="flex ml-2">
-                  <input type="checkbox" />
-                  <div className="flex ml-2">
-                    <span className={`font-medium text-black`}>Item</span>
-                  </div>
-                </div>
-                <div className="flex items-center">
-                  <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-1">
-                    Edit
-                  </button>
-                  <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                    Delete
-                  </button>
-                </div>
-              </li>
-            </ul>
+            {items.length ? (
+              <ul>
+                {items.map((item) => (
+                  <li className="flex justify-between items-center bg-white mt-2 p-2 hover:shadow-lg rounded transition"
+                  key={item.id}>
+                    <div className="flex ml-2">
+                      <input type="checkbox" checked={item.checked}/>
+                      <div className="flex ml-2">
+                        <span className={`font-medium text-black ${item.checked ? 'line-through' : null}`}>
+                          {item.task}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="flex items-center">
+                      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-1">
+                        Edit
+                      </button>
+                      <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                        Delete
+                      </button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <h1>Your list is empty.</h1>
+            )}
           </div>
         </div>
       </div>
